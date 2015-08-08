f924c8dc9d30b166a70b657938c4cba2d7fa2a1f
JacksonLLee <jsllee.phon@gmail.com>
2015-08-08 14:08:32 -0500

    improve user interface of neighbors.py

--- neighbors.py
+++ neighbors.py
@@ -19,6 +19,8 @@ from lxa5lib import (get_language_corpus_datafolder,
 import manifold
 
 # colors for generations of nodes in output graph
+# RGB_list[0] is the color for the seed word node
+# RGB_list[1] is the color for the 1st generation word nodes, and so forth
 RGB_list = [{'a': 1, 'r': 255, 'g': 128, 'b': 0}, # orange
             {'a': 1, 'r': 51, 'g': 153, 'b': 255}, # blue
             {'a': 1, 'r': 255, 'g': 255, 'b': 0}, # yellow
@@ -26,6 +28,9 @@ RGB_list = [{'a': 1, 'r': 255, 'g': 128, 'b': 0}, # orange
             {'a': 1, 'r': 255, 'g': 51, 'b': 51}, # red
            ]
 
+# the number of generations is restricted by len_RGB_list
+# (if we want more generations, we'll just have to build in more colors in RGB_list...)
+len_RGB_list = len(RGB_list)
 
 def makeArgParser(configfilename="config.json"):
 
@@ -86,8 +91,6 @@ def main(language, corpus, datafolder):
 
         max_length_filename = max([len(x.name) for x in gexf_infilenames])
 
-
-
         while True:
             gexf_choice = input("\nChoose from the following .gexf neighbor files\n"
                               "by entering the number:\n"
@@ -118,7 +121,7 @@ def main(language, corpus, datafolder):
     while True:
         if word_generations_list:
             current_words = "\n\nCurrent words and their generations: " + \
-                            "(You may add another word.)\n" + \
+                            "(You may add or delete words.)\n" + \
                             "\n".join(["{} {}".format(w,g)
                                        for w, g in word_generations_list])
         else:
@@ -127,47 +130,41 @@ def main(language, corpus, datafolder):
         userinput = input("-----------------------------\n"
                           "What word(s) do you want to explore? "
                           "Input options:\n"
-                          "1. \"<word> <k>\"\n"
+                          "1. \"add <word> <k>\" -- Add this word for output.\n"
                           "          <word> = the word\n"
-                          "          <k> = how many generations (> 0) from that word.\n"
-                          "2. \"go\" -- Create the neighbor graph for the current words.\n"
-                          "3. \"exit\" -- Exit.{}\n\n>>> ".format(current_words))
+                          "          <k> = how many generations (0 < k < {}) "
+                          "from that word.\n"
+                          "2. \"del <word>\" -- Delete this word.\n"
+                          "3. \"run\" -- Create the neighbor graph for the words added.\n"
+                          "4. \"exit\" -- Exit."
+                          "{}\n\n>>> ".format(len_RGB_list, current_words))
 
         userinput = userinput.strip().casefold()
 
-        try:
-            # check if input is an actual word plus number of generations
-            word, k = userinput.split()
-            if word not in wordlist:
-                print("")
-                raise ValueError
+        # check if user input is valid with a legal command word
+        command, *rest = userinput.split()
 
-            k = int(k)
+        if command not in {"add", "del", "run", "exit"}:
+            print("Invalid command word: {}".format(command))
+            continue
 
-            # can take away "k > 4" when we can accommodate more colors!
-            if k < 1 or k > 4:
-                print("Invalid number of generations -- "
-                      "it must be positive and smaller than 5.")
-                raise ValueError
-
-            # if both word and k pass the tests above, keep them.
-            word_generations_list.append((word, k))
-
-        except (ValueError, IndexError):
-            if userinput not in {"go", "exit"}:
-                print("Invalid input\n")
-                continue
-            elif userinput == "go" and not word_generations_list:
-                print("No words are chosen.")
-                continue
-
-        if userinput == "exit":
+        # perform the appropriate actions according to the command word
+        if command == "exit":
             sys.exit("Program terminated by user.")
-        elif userinput == "go":
+
+        elif command == "run":
+
+            # make sure there are words in word_generations_list
+            if not word_generations_list:
+                print("No words are selected.")
+                continue
+
             print("The program now creates the neighbor graph.\n")
 
+            # initialize the output graph
             output_G = nx.Graph()
 
+            # add nodes and edges to output graph; color nodes by generations
             for word, generations in word_generations_list:
                 current_words = {word} # a set
                 current_generation = 0
@@ -198,16 +195,82 @@ def main(language, corpus, datafolder):
                     current_words = next_batch
                     current_generation += 1
 
+            # force all seed words to have the color for seed words
+            for word, generations in word_generations_list:
+                output_G.node[word]['viz']['color'] = RGB_list[0]
+
+            # output the graph as .gexf
             filenamesuffix = "_".join([w+"-"+str(g)
                                        for w,g in word_generations_list]) + ".gexf"
             outgraphfilename = gexf_infilename.stem.replace("neighbors",
                                                             filenamesuffix)
-            nx.write_gexf(output_G, str(Path(outfolder, outgraphfilename)))
+            outgraph_path = Path(outfolder, outgraphfilename)
+            nx.write_gexf(output_G, str(outgraph_path))
 
             print("===================================\n\n"
-                  "Neighbor graph generated:\n{}\n".format(outgraphfilename))
+                  "Neighbor graph generated:\n{}\n".format(outgraph_path))
+
+            print("Note:\n"
+                  "1. All seed words are forced to bear the color for seed words,\n"
+                  "   so that we can visually see the distances between seed words.\n"
+                  "2. The ordering by which seed words were entered influences\n"
+                  "   the coloring of nodes, because currently the code does not\n"
+                  "   allow overrding the color of a node already in the output\n"
+                  "   graph. Try the same words with different orders.\n")
 
             break
+
+        elif command == "add":
+
+            word, *k = rest
+
+            # check if word and k (= number of generations) are valid
+            if word not in wordlist:
+                print("The word is not in the neighbor graph.")
+                continue
+
+            try:
+                k = int(k[0])
+                if k < 1 or k >= len_RGB_list:
+                    raise ValueError
+            except (ValueError, IndexError):
+                print("Invalid number of generations -- "
+                      "it must be a positive interger "
+                      "smaller than {}.".format(len_RGB_list))
+                continue
+
+            # check if word is already in word_generations_list
+            current_words = {w for w, k in word_generations_list} # a set
+            if word in current_words:
+                print("The word is already added.")
+                continue
+
+            # if all tests above are passed, add (word, k) to word_generations_list
+            word_generations_list.append((word, k))
+
+        elif command == "del":
+
+            # check if there's a word in the user input
+            try:
+                word = rest[0]
+            except IndexError:
+                print("No word is given.")
+                continue
+
+            # check if word is absent from word_generations_list
+            current_words = {w for w, k in word_generations_list} # a set
+            if word not in current_words:
+                print("The word has not been added.")
+                continue
+
+            # if all tests above are passed, delete (word, k) in word_generations_list
+
+            for _word, k in word_generations_list[:]:
+            # the [:] syntax creates a copy as loop iterator,
+            # which allows us to delete items from word_generations_list inside the loop
+                if _word == word:
+                    word_generations_list.remove((word, k))
+                    break
 
 
 if __name__ == "__main__":
